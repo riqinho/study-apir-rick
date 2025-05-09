a8c9fd1d0d42ac82d341e5ecc6268e60f17713d0
Rick <rickadomingues@gmail.com>
2025-05-08 22:50:47 -0300

    feat: comecando com findByStatus

--- src/main/java/com/github/acnaweb/study_apir/repository/PedidoRepository.java
+++ src/main/java/com/github/acnaweb/study_apir/repository/PedidoRepository.java
@@ -1,5 +1,7 @@
 package com.github.acnaweb.study_apir.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.github.acnaweb.study_apir.model.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
-
+    public List<Pedido> findByStatus(String status); // Método para buscar pedidos por status
 }
